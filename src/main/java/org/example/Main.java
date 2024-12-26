60ab7064686db89ae7a11ba50cd70ddf98531ecf
Aayu Parikh <aryanparikh03@gmail.com>
2024-12-26 16:25:20 +0530

    Decorator pattern has been implemented

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,109 @@
 package org.example;
 
+// Component
+interface Coffee {
+    String getDescription();
+    double getCost();
+}
+
+// Concrete Component
+class BasicCoffee implements Coffee {
+    @Override
+    public String getDescription() {
+        return "Basic Coffee";
+    }
+
+    @Override
+    public double getCost() {
+        return 2.0; // Base price for coffee
+    }
+}
+
+// Decorator
+abstract class CoffeeDecorator implements Coffee {
+    protected Coffee coffee;
+
+    public CoffeeDecorator(Coffee coffee) {
+        this.coffee = coffee;
+    }
+
+    @Override
+    public String getDescription() {
+        return coffee.getDescription();
+    }
+
+    @Override
+    public double getCost() {
+        return coffee.getCost();
+    }
+}
+
+// Concrete Decorators
+class MilkDecorator extends CoffeeDecorator {
+    public MilkDecorator(Coffee coffee) {
+        super(coffee);
+    }
+
+    @Override
+    public String getDescription() {
+        return coffee.getDescription() + ", Milk";
+    }
+
+    @Override
+    public double getCost() {
+        return coffee.getCost() + 0.5; // Adding cost of milk
+    }
+}
+
+class SugarDecorator extends CoffeeDecorator {
+    public SugarDecorator(Coffee coffee) {
+        super(coffee);
+    }
+
+    @Override
+    public String getDescription() {
+        return coffee.getDescription() + ", Sugar";
+    }
+
+    @Override
+    public double getCost() {
+        return coffee.getCost() + 0.2; // Adding cost of sugar
+    }
+}
+
+class CaramelDecorator extends CoffeeDecorator {
+    public CaramelDecorator(Coffee coffee) {
+        super(coffee);
+    }
+
+    @Override
+    public String getDescription() {
+        return coffee.getDescription() + ", Caramel";
+    }
+
+    @Override
+    public double getCost() {
+        return coffee.getCost() + 0.8; // Adding cost of caramel
+    }
+}
+
+// Client
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Start with a basic coffee
+        Coffee coffee = new BasicCoffee();
+        System.out.println(coffee.getDescription() + " -> $" + coffee.getCost());
+
+        // Add milk
+        coffee = new MilkDecorator(coffee);
+        System.out.println(coffee.getDescription() + " -> $" + coffee.getCost());
+
+        // Add sugar
+        coffee = new SugarDecorator(coffee);
+        System.out.println(coffee.getDescription() + " -> $" + coffee.getCost());
+
+        // Add caramel
+        coffee = new CaramelDecorator(coffee);
+        System.out.println(coffee.getDescription() + " -> $" + coffee.getCost());
     }
 }
