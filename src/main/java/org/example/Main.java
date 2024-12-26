32b137eb8dd36d8ed47f03f1219610ae7e0fd7e1
Aayu Parikh <aryanparikh03@gmail.com>
2024-12-26 15:02:28 +0530

    Bridge pattern is implemented

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,104 @@
 package org.example;
 
+// Implementor Interface
+interface Device {
+    void turnOn();
+    void turnOff();
+    void setVolume(int volume);
+}
+
+// Concrete Implementors
+class TV implements Device {
+    private int volume;
+
+    @Override
+    public void turnOn() {
+        System.out.println("Turning on the TV.");
+    }
+
+    @Override
+    public void turnOff() {
+        System.out.println("Turning off the TV.");
+    }
+
+    @Override
+    public void setVolume(int volume) {
+        this.volume = volume;
+        System.out.println("TV volume set to " + volume);
+    }
+}
+
+class Radio implements Device {
+    private int volume;
+
+    @Override
+    public void turnOn() {
+        System.out.println("Turning on the Radio.");
+    }
+
+    @Override
+    public void turnOff() {
+        System.out.println("Turning off the Radio.");
+    }
+
+    @Override
+    public void setVolume(int volume) {
+        this.volume = volume;
+        System.out.println("Radio volume set to " + volume);
+    }
+}
+
+// Abstraction
+abstract class RemoteControl {
+    protected Device device;
+
+    public RemoteControl(Device device) {
+        this.device = device;
+    }
+
+    public void turnOn() {
+        device.turnOn();
+    }
+
+    public void turnOff() {
+        device.turnOff();
+    }
+
+    public void setVolume(int volume) {
+        device.setVolume(volume);
+    }
+}
+
+// Refined Abstraction
+class AdvancedRemoteControl extends RemoteControl {
+    public AdvancedRemoteControl(Device device) {
+        super(device);
+    }
+
+    public void mute() {
+        System.out.println("Muting the device.");
+        device.setVolume(0);
+    }
+}
+
+// Client Code
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // TV with Basic Remote
+        Device tv = new TV();
+        RemoteControl tvRemote = new RemoteControl(tv){};
+        tvRemote.turnOn();
+        tvRemote.setVolume(15);
+        tvRemote.turnOff();
+
+        System.out.println();
+
+        // Radio with Advanced Remote
+        Device radio = new Radio();
+        AdvancedRemoteControl radioRemote = new AdvancedRemoteControl(radio);
+        radioRemote.turnOn();
+        radioRemote.setVolume(20);
+        radioRemote.mute();
+        radioRemote.turnOff();
     }
 }
