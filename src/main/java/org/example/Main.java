7b4e75c66338f009a0037945a7f2c6fd6e728d9d
Aayu Parikh <aryanparikh03@gmail.com>
2024-12-27 11:12:10 +0530

    Facade design pattern code is being implemented

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,53 @@
 package org.example;
 
+// Subsystems
+class CPU {
+    public void freeze() {
+        System.out.println("CPU freezing...");
+    }
+
+    public void execute() {
+        System.out.println("CPU executing instructions...");
+    }
+}
+
+class Memory {
+    public void load(long position, String data) {
+        System.out.println("Loading data into memory at position " + position + ": " + data);
+    }
+}
+
+class HardDrive {
+    public String read(long lba, int size) {
+        return "Boot data";
+    }
+}
+
+// Facade
+class ComputerFacade {
+    private CPU cpu;
+    private Memory memory;
+    private HardDrive hardDrive;
+
+    public ComputerFacade() {
+        this.cpu = new CPU();
+        this.memory = new Memory();
+        this.hardDrive = new HardDrive();
+    }
+
+    public void startComputer() {
+        System.out.println("Starting the computer...");
+        cpu.freeze();
+        String bootData = hardDrive.read(0, 512);
+        memory.load(0, bootData);
+        cpu.execute();
+    }
+}
+
+// Client
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ComputerFacade computer = new ComputerFacade();
+        computer.startComputer();
     }
 }
